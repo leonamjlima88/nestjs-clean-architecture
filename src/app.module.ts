f0892884b2afdbe1ac193c3fdf857b03e5ee52ab
Leonam J. Lima <106016830+leonamjlima88@users.noreply.github.com>
2022-06-30 19:22:05 -0300

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { UserRepository } from './core/repositories/user.repository';
+import { UsersCacheMemoryRepository } from './data/cache-memory/users-cache-memory.repository';
+import { UsersControllers } from './presentation/users.controller';
+import { CreateUserUseCase } from './use-cases/create-user.usecase';
+import { GetAllUsersUseCase } from './use-cases/get-all-users.usecase';
+
+@Module({
+  imports: [],
+  controllers: [UsersControllers],
+  providers: [
+    {
+      provide: UserRepository,
+      useClass: UsersCacheMemoryRepository,
+    },
+    CreateUserUseCase,
+    GetAllUsersUseCase,
+  ],  
+})
+export class AppModule { }
